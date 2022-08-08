6fb701dcabd1e21fb03c1942cd1d68cffa891230
welly091 <whtp1490@gmail.com>
2022-08-08 16:47:45 -0700

    reset

--- api/data.py
+++ api/data.py
@@ -1,6 +1,5 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
-from urllib import parse
 
 class handler(BaseHTTPRequestHandler):
 
@@ -8,26 +7,7 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    message = "Howdy"
-    self.wfile.write(message.encode())
-
-    #
-    # url_components = parse.urlsplit(self.path)
-    # query_string_list = parse.parse_qsl(url_components.query)
-    # query_dic = dict(query_string_list)
-
-    # url = "url"
-    # response = requests.get(url)
-    # data = response.json()
-    # definition = []
-
-
-    # friend = query_dic["friend"]
-
-    # message=str(url_components)
-    # self.wfile.write(message.encode().query)
-    #self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
     return
-
 
 
