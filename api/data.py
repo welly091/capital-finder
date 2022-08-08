fd353340a8795ff09c42bff211e842509a005304
welly091 <whtp1490@gmail.com>
2022-08-08 16:14:30 -0700

    print message howdy

--- api/data.py
+++ api/data.py
@@ -8,10 +8,26 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-
-    url_components = parse.urlsplit(self.path)
-
-    message=str(url_components)
+    message = "Howdy"
     self.wfile.write(message.encode())
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+
+    #
+    # url_components = parse.urlsplit(self.path)
+    # query_string_list = parse.parse_qsl(url_components.query)
+    # query_dic = dict(query_string_list)
+
+    # url = "url"
+    # response = requests.get(url)
+    # data = response.json()
+    # definition = []
+
+
+    # friend = query_dic["friend"]
+
+    # message=str(url_components)
+    # self.wfile.write(message.encode().query)
+    #self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
     return
+
+
+
