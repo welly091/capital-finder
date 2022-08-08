f9018487294fe43ffd566c859cc9db35bea34e05
welly091 <whtp1490@gmail.com>
2022-08-08 11:39:46 -0700

    second commit

--- api/data.py
+++ api/data.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
+from urllib import parse
 
 class handler(BaseHTTPRequestHandler):
 
@@ -7,5 +8,10 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
+
+    url_components = parse.urlsplit(self.path)
+
+    message=str(url_components)
+    self.wfile.write(message.encode())
     self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
     return
